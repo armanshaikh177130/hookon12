4a9b15823da91d910187101ef19868f9a75b8ae3
armanshaikh177130 <52879080+armanshaikh177130@users.noreply.github.com>
2019-07-17 17:25:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ client.on("message", async message => {
     // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
     message.delete().catch(O_o=>{}); 
     // And we get the bot to say the thing: 
-    message.channel.send(random);
+    message.channel.send();
   }
   
   if(command === "kick") {
